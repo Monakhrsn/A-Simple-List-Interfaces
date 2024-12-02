61738a30326bb16a6236a5e555ea1275d3bc3ae2
Mona Khorasani <monakhrsn@gmail.com>
2024-12-02 21:02:30 +0100

    Fix

--- MainApp/obj/Debug/net9.0/MainApp.AssemblyInfo.cs
+++ MainApp/obj/Debug/net9.0/MainApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MainApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8f54c649fd61da592011988819936403f1cabec")]
 [assembly: System.Reflection.AssemblyProductAttribute("MainApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MainApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
